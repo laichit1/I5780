531384abb58f514339a9f9bcb0022f1b91254c45
laichit1 <43901023+laichit1@users.noreply.github.com>
2018-12-28 17:27:47 +0800

    Update hw6/main.js

--- hw6/main.js
+++ hw6/main.js
@@ -42,12 +42,6 @@ function init() {
 
 
   ///////////////////////
-    var sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    timer = setInterval( function(){
-      $("#seconds").html(pad(++sec%60));
-      $("#minutes").html(pad(parseInt(sec/60,10)));
-    }, 1000);
 
 }
 
