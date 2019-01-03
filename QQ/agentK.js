fd9ba8df55a15b86f8d3e4534a0fc0853e1ef7a5
laichit1 <43901023+laichit1@users.noreply.github.com>
2019-01-04 00:24:12 +0800

    2018/01/03

--- QQ/agentK.js
+++ QQ/agentK.js
@@ -30,7 +30,7 @@ class Agent {
     this.halfSize = halfSize;  // half width
     this.mesh = agentMesh (this.halfSize, 'cyan');
     this.MAXSPEED = 800;
-    this.ARRIVAL_R = 74;
+    this.ARRIVAL_R = 73;
     
     this.score = 0;
     
