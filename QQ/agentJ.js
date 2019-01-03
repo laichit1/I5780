9d504a105c6bf2412f1d6f603a7b999e1221f3ce
laichit1 <43901023+laichit1@users.noreply.github.com>
2019-01-03 22:38:29 +0800

    Update QQ/agentJ.js

--- QQ/agentJ.js
+++ QQ/agentJ.js
@@ -30,7 +30,7 @@ class Agent {
     this.halfSize = halfSize;  // half width
     this.mesh = agentMesh (this.halfSize, 'red');
     this.MAXSPEED = 800;
-    this.ARRIVAL_R = 73;
+    this.ARRIVAL_R = 74;
     
     this.score = 0;
     
